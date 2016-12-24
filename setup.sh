90f700b22dcf96a55e4ce9e826141cb09d6f9d0c
browles <rowlesben@gmail.com>
2016-12-24 15:44:46 -0800

    Use `stat` to test for dotfile before creating link

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@ dir="$HOME/Dropbox/dotfiles"
 dotfiles=".emacs.d .ssh .gitconfig .zshrc .tmux.conf"
 
 for file in $dotfiles; do
-  echo Creating link to $dir/$file at $HOME/$file
-  ln -s $dir/$file $HOME/$file
+  if stat $HOME/$file > /dev/null 2>&1; then
+    echo $HOME/$file already exists.
+  else
+    echo Creating link to $dir/$file at $HOME/$file
+    ln -s $dir/$file $HOME/$file
+  fi
 done
