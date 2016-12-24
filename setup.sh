1f811213d2e5b1830389e02f722ffa5900da064b
browles <rowlesben@gmail.com>
2016-12-24 15:46:13 -0800

    Use explicit strings for `echo`

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ dotfiles=".emacs.d .ssh .gitconfig .zshrc .tmux.conf"
 
 for file in $dotfiles; do
   if stat $HOME/$file > /dev/null 2>&1; then
-    echo $HOME/$file already exists.
+    echo "$HOME/$file already exists."
   else
-    echo Creating link to $dir/$file at $HOME/$file
+    echo "Creating link to $dir/$file at $HOME/$file."
     ln -s $dir/$file $HOME/$file
   fi
 done
