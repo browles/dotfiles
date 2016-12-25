cb5ad32a9e7181fe1d5eea5a916e2e68797a5d2c
browles <rowlesben@gmail.com>
2016-12-24 16:08:16 -0800

    Use proper bash conditional

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ dir="$HOME/Dropbox/dotfiles"
 dotfiles=".emacs.d .ssh .gitconfig .zshrc .tmux.conf"
 
 for file in $dotfiles; do
-  if stat $HOME/$file > /dev/null 2>&1; then
+  if [ -a $HOME/$file ]; then
     echo "$HOME/$file already exists."
   else
     echo "Creating link to $dir/$file at $HOME/$file."
