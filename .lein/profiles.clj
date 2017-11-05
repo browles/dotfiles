289fa60ec8fbac2da05e7422a3705d3a77b7c25d
browles <rowlesben@gmail.com>
2017-11-04 19:27:28 -0700

    misc

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,5 @@
-{:user
-   {:plugins [[cider/cider-nrepl "0.15.0"]
-              [lein-ancient "0.6.10"]
-              [lein-pprint "1.1.2"]
-              [refactor-nrepl "2.0.0"]]
-    :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.15.0"]
+                  [lein-ancient "0.6.10"]
+                  [lein-pprint "1.1.2"]
+                  #_[refactor-nrepl "2.0.0"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
--- .zshrc
+++ .zshrc
@@ -5,7 +5,7 @@ plugins=(git autojump osx)
 alias emacs-kill="emacsclient -e \"(kill-emacs)\""
 alias ec="emacsclient --no-wait"
 alias octave=octave-cli
-export ZSH=~/.oh-my-zsh
+type google-chrome &>/dev/null && alias chrome=google-chrome
 source $ZSH/oh-my-zsh.sh
 
 eval "$(rbenv init -)"
