ca1b5633a27d2fdc30ea385bd48b39c68c83cc4c
browles <rowlesben@gmail.com>
2023-10-30 14:37:10 -0700

    Use generic "prefix" and specify weather city
    Not specifying the location is currently giving an inaccurate temp

--- config/.config/tmux/weather.sh
+++ config/.config/tmux/weather.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-url="wttr.in?format=%t+%c+%l"
+url="wttr.in/Seattle?format=%t+%c+%l"
 cache=~/.cache/wttr-report.txt
 # If cache does not exist or is older than 5m
 if ! [[ $(find $cache -mtime -5m 2>/dev/null) ]]; then
