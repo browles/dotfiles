54604b5b7b1b350350c8b156c06fad16660abd9e
browles <rowlesben@gmail.com>
2023-10-31 19:47:49 -0700

    Simplify tmux-status-right

--- config/.config/tmux/tmux-status-right.sh
+++ config/.config/tmux/tmux-status-right.sh
@@ -2,8 +2,8 @@
 set -eu -o pipefail
 cols=$(tmux display-message -p "#{window_width}")
 
-if [[ cols -lt 150 ]]; then
-  echo "$(~/.config/tmux/battery.sh) | $(~/.config/tmux/weather.sh) | $(date -u +'%Y-%m-%d %H:%M %Z')"
-else
-  echo "$(~/.config/tmux/tmux-mem-cpu-load.sh) | $(~/.config/tmux/battery.sh) | $(~/.config/tmux/weather.sh) | $(date -u +'%Y-%m-%d %H:%M %Z')"
+right="$(~/.config/tmux/battery.sh) | $(~/.config/tmux/weather.sh) | $(date -u +'%Y-%m-%d %H:%M %Z')"
+if [[ cols -gt 150 ]]; then
+  right="$(~/.config/tmux/tmux-mem-cpu-load.sh) | $right"
 fi
+echo $right
