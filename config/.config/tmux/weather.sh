37396b976e1a15e2e54eddd37ebeb906628a6ec2
browles <rowlesben@gmail.com>
2023-11-03 01:31:37 -0700

    Fix find usage for GNU find, remove weather emoji

--- config/.config/tmux/weather.sh
+++ config/.config/tmux/weather.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail
 
-url="wttr.in/Seattle?format=%t+%c+%l"
+url="wttr.in/Seattle?format=%t+%l"
 cache=~/.cache/wttr-report.txt
 # If cache does not exist or is older than 5m
-if ! [[ $(find $cache -mtime -5m 2>/dev/null) ]]; then
+if ! [[ $(find $cache -mmin -5 2>/dev/null) ]]; then
   curl -sS --compressed "$url" > "$cache"
 fi
 
-awk 'BEGIN {FS = ","}; {print $1}' < "$cache" | sed 's/   /  /g'
+awk 'BEGIN {FS = ","}; {print $1}' < "$cache"
