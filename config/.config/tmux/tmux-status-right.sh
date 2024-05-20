0ae3e97a49db7102d46ba263a7d394b9d308c27c
browles <rowlesben@gmail.com>
2024-05-20 03:49:29 +0000

    Add user@host to status right

--- config/.config/tmux/tmux-status-right.sh
+++ config/.config/tmux/tmux-status-right.sh
@@ -4,10 +4,10 @@ cols=$(tmux display-message -p "#{window_width}")
 
 uname=$(uname)
 right=""
-if [[ uname == "Darwin" ]]; then
+if [[ "$uname" == "Darwin" ]]; then
   right="$(~/.config/tmux/battery.sh) | $(~/.config/tmux/weather.sh) | $(date -u +'%Y-%m-%d %H:%M %Z')"
 else
-  right="$(~/.config/tmux/weather.sh) | $(date -u +'%Y-%m-%d %H:%M %Z')"
+  right="$(~/.config/tmux/weather.sh) | #{user}@#{host} | $(date -u +'%Y-%m-%d %H:%M %Z')"
 fi
 if [[ cols -gt 150 ]]; then
   right="$(~/.config/tmux/tmux-mem-cpu-load.sh) | $right"
